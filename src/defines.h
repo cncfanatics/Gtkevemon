43c2b8a3724d7c32db8f795af755d7bd2136b16b
Simon <Simon@6e31b030-b45c-3146-9160-cf88ccb029a2>
2009-03-08 16:28:45 +0000

    Makefile.dep update
    git-svn-id: svn://gtkevemon.battleclinic.com/GTKEVEMon/trunk/gtkevemon@90 6e31b030-b45c-3146-9160-cf88ccb029a2

--- src/defines.h
+++ src/defines.h
@@ -13,7 +13,7 @@
 #ifndef DEFINES_HEADER
 #define DEFINES_HEADER
 
-#define GTKEVEMON_VERSION_STR "Revision 1.6-88"
+#define GTKEVEMON_VERSION_STR "Revision 1.6-90"
 #define LAUNCHER_CMD_AMOUNT 5
 
 #endif /* DEFINES_HEADER */
